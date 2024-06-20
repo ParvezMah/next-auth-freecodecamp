bbc4033a8b1a6620053d6b4219c016da175eec57
Parvez Mahamud AA <parvezmahmudaa100@gmail.com>
2024-06-20 08:56:22 +0600

    added Protection of Server Side Page

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -24,6 +24,8 @@ export const options = {
             profile(profile){
                 console.log("Profile Google", profile);
 
+                let userRole = "Google User";
+
                 return {
                     ...profile,
                     id: profile.sub,
